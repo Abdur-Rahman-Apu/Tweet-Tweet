3ea4a351ace18ada1988e9e362a88406013b9b3e
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-20 21:15:44 +0600

    localstorage related utility functions are created

---
+++ utils/storage.js
@@ -0,0 +1,15 @@
+/*
+setIntoLocalStorage: Set something into local storage
+Parameter: key and value
+*/
+const setIntoLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+/*
+getFromLocalStorage: Get something from the local storage
+Parameter: key 
+*/
+const getFromLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
