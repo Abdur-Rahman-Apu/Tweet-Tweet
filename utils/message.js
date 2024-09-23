4a46e46c1b537f3a3d6ded235fed38758f215ceb
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-09-23 08:39:57 +0600

    Message file of the util folder cleaned and added necessary comments

--- utils/message.js
+++ utils/message.js
@@ -4,23 +4,34 @@ import {
   setApplicationData,
 } from "../lib/Storage/storage.js";
 
+// To get message id: Just make the date into 9-2-2024 format
 const getMessageId = (date) => {
   return date.split("/").join("-");
 };
 
+// get messages of the specific date
 const getSpecificDateMessages = (date) => {
   const { messages } = getApplicationData();
-  console.log(date, "getSpecificDateMessages");
-  console.log(messages, "getSpecificDateMessages");
   return messages[date];
 };
 
+// set specific date messages and store into the storage
 const setSpecificDateMessagesIntoStorage = (date, messages) => {
   const data = getApplicationData();
   data["messages"][date] = messages;
   setApplicationData(data);
 };
 
+/*
+setTextStyle: To make the text bold, italic, underline and line through
+
+Parameters:
+-----------
+tag: b,i,u,s
+msg: select message
+startIndex: select message's start index
+endIndex: select message's end index
+*/
 const setTextStyle = (tag, msg, startIndex, endIndex) => {
   const charArr = msg.split("");
 
@@ -30,6 +41,7 @@ const setTextStyle = (tag, msg, startIndex, endIndex) => {
   typeMsg.value = charArr.join("");
 };
 
+// Obtain the input box message length
 const getMessageLength = () => {
   return Array.from(typeMsg.value).length;
 };
